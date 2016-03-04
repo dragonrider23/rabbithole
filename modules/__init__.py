f3b5592e8ee57dac683077f02a0cb0207b95c719
Lee Keitel <lee@keitel.xyz>
2016-03-04 14:35:56 -0600

    We only need to look for .py files, no .py means module not loaded

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,10 +3,7 @@ import glob
 __all__ = []
 
 modules = glob.glob(dirname(__file__)+"/*.py")
-modules.extend(glob.glob(dirname(__file__)+"/*.pyc"))
-modules.extend(glob.glob(dirname(__file__)+"/*.pyo"))
 for f in modules:
-    fn = basename(f)
-    fn = fn.rsplit('.', 1)[0]
+    fn = basename(f).rsplit('.', 1)[0]
     if isfile(f) and not fn.startswith('__') and not fn in __all__:
         __all__.append(fn)
