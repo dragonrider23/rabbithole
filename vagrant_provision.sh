e7e7b5f43da60d0dcb538c6c98d1c2043716b18a
Lee Keitel <lee@keitel.xyz>
2016-03-04 22:02:55 -0600

    Updated vagrant provisioning script

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,18 +1,19 @@
 #! /usr/bin/env bash
 
-# Configure SSHD
-echo -e "\n"'Match User !vagrant,*' >> /etc/ssh/sshd_config
-echo "ForceCommand /vagrant/rabbithole.py" >> /etc/ssh/sshd_config
-service ssh restart
-
 # Create test user
 adduser --disabled-password --gecos "" tester
 echo "tester:a" | chpasswd
 
 # Configure rabbithole
 mkdir /etc/rabbithole
-cp /vagrant/rabbithole.cfg /etc/rabbithole/rabbithole.cfg
+cp /vagrant/rabbithole.cfg.sample /vagrant/rabbithole.cfg.vagrant
+ln -s /vagrant/rabbithole.cfg.vagrant /etc/rabbithole/rabbithole.cfg
 cp /vagrant/inventory /etc/rabbithole/inventory
 
 # Link to command in PATH
 ln -s /vagrant/rabbithole.py /usr/local/bin/rabbithole
+
+# Configure SSHD
+echo -e "\n"'Match User !vagrant,*' >> /etc/ssh/sshd_config
+echo "ForceCommand /usr/local/bin/rabbithole" >> /etc/ssh/sshd_config
+service ssh restart
